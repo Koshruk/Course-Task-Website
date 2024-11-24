acd8d072feaa103fccd262ec94a08e8b3bfeaebb
Koshruk <145700663+Koshruk@users.noreply.github.com>
2024-11-24 16:55:38 +0200

    started json data work

---
+++ js/generate-data.js
@@ -0,0 +1,9 @@
+const offerList = document.querySelector
+
+fetch('../data/offers.json')
+    .then(res => res.json())
+    .then(data => {
+        data.array.forEach(offer => {
+            
+        });
+    });
